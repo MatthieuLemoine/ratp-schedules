1d60ec30c589ba47f43bb9e19288ce5c44c3294a
MatthieuLemoine <MatthieuLemoine@users.noreply.github.com>
2018-11-18 21:47:41 +0100

    :sparkles: Mobile

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import { AppRegistry } from 'react-native';
+import Application from './src/components/Test';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => Application);
--- server/logger/index.js
+++ server/logger/index.js
@@ -1,10 +1,12 @@
 const chalk = require('chalk');
 
-const getLogger = (level, colorizer, logger) => arg => logger(`${colorizer(`[${level}]`)}: ${arg}`);
+const getLogger = (level, colorizer, logger) => (...args) =>
+  logger(colorizer(`[${level}]:`), ...args);
 
 module.exports = {
   error: getLogger('error', chalk.red, console.error),
   warn: getLogger('warn', chalk.yellow, console.warn),
   info: getLogger('info', chalk.green, console.info),
   debug: getLogger('debug', chalk.blue, console.log),
+  log: console.log,
 };
